23e99b29d63c0ad80f74e6f7de991265c5c76066
yanxiong <oujinhui.ojh@antgroup.com>
2023-12-26 19:50:17 +0800

    build: add deploy script to project

--- .umirc.ts
+++ .umirc.ts
@@ -3,4 +3,6 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   routes: [{ path: '/', component: 'index' }],
   npmClient: 'yarn',
+  base: '/murder-mystery-counterfoil-maker',
+  publicPath: '/murder-mystery-counterfoil-maker/',
 });
